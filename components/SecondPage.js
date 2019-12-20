8b5f8efb4b7bef196b5d4830a0f4a9edc502cda0
Andre711 <andrej.pavlovcic@gmail.com>
2019-12-20 16:59:49 +0100

    gump NEXT

--- components/SecondPage.js
+++ components/SecondPage.js
@@ -1,41 +1,47 @@
-import React, { Component } from 'react';
-import { StyleSheet, TextInput } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, TextInput} from 'react-native';
 import Moment from 'moment';
 
 import {
-    Layout,
-    Text,
-    Icon,
-    TopNavigation,
-    TopNavigationAction,
-    Spinner,
-    Button,
+  Layout,
+  Text,
+  Icon,
+  TopNavigation,
+  TopNavigationAction,
+  Spinner,
+  Button,
 } from '@ui-kitten/components';
 
-import { Actions } from 'react-native-router-flux';
+import {Actions} from 'react-native-router-flux';
 import CommentsSection from './CommentsSection';
 import Tag from './Tag';
 import moment from 'moment';
 
-import { AsyncStorage } from 'react-native';
+import {AsyncStorage} from 'react-native';
 
 function get_item() {
-    AsyncStorage.getItem('userId', (err, result) => {
-        console.log(result);
-    });
+  AsyncStorage.getItem('userId', (err, result) => {
+    console.log(result);
+  });
 }
 
 export default class Login extends Component {
-
-    render() {
-        return (
-            <Layout>
-                <Button
-                    onPress={() => {
-                        get_item();
-                    }}>TEST</Button>
-            </Layout>
-        );
-    }
-
+  render() {
+    return (
+      <Layout>
+        <Button
+          onPress={() => {
+            get_item();
+          }}>
+          TEST
+        </Button>
+        <Button
+          onPress={() => {
+            Actions.mainPage();
+          }}>
+          NEXT
+        </Button>
+      </Layout>
+    );
+  }
 }
